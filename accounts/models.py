b46245af0c9120f4333fd170c958b21a7cb9aa3c
Mitesh Patel <developermitesh@gmail.com>
2018-06-09 20:33:05 +0530

    First commit of the Django code.

---
+++ accounts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class UserProfile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  description = models.CharField(max_length=100, default= '')
+  city = models.CharField(max_length=100, default='')
+  website = models.URLField()
+  phone = models.IntegerField(default=0)
