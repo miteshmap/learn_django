50242bf0882476f9a4415698800997a0da96670d
Mitesh Patel <developermitesh@gmail.com>
2018-06-13 20:35:33 +0530

    Updated models to create user profile on user creation.

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
-# Create your models here.
 class UserProfile(models.Model):
+  """
+  User profile class to have additional fields for users.
+  """
   user = models.OneToOneField(User, on_delete=models.CASCADE)
   description = models.CharField(max_length=100, default= '')
   city = models.CharField(max_length=100, default='')
   website = models.URLField()
   phone = models.IntegerField(default=0)
+
+"""
+Create user profile when admin creates a user that's why it should
+be triggered in post_save of User.
+"""
+def create_profile(sender, **kwargs):
+  if kwargs['created']:
+    user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+post_save.connect(create_profile, sender=User)
